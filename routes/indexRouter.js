68a7001c3e28304bcd913a36fdb31f43a9f4ece8
Caleb Steinwandt <jsteinca@jci.com>
2025-05-12 11:03:19 -0600

    build: basic message board functionality

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,5 +1,22 @@
 const { Router } = require('express');
 
+const messages = [
+    {
+      text: "Hi there!",
+      user: "Amando",
+      added: new Date()
+    },
+    {
+      text: "Hello World!",
+      user: "Charles",
+      added: new Date()
+    }
+  ];
+
 const indexRouter = Router();
 
-module.exports = indexRouter;
+indexRouter.get('/', (req, res) => {
+    res.render('index', { title: 'Mini Message Board', messages: messages})
+})
+
+module.exports = {indexRouter, messages};
--- routes/newMessageRouter.js
+++ routes/newMessageRouter.js
@@ -1,5 +1,17 @@
 const { Router } = require('express');
+// const express = require('express');
+const {messages} = require('./indexRouter')
 
 const newMessageRouter = Router();
+newMessageRouter.get('/', (req, res) => {
+    res.render('form');
+})
+
+console.log(messages)
+newMessageRouter.post('/', (req, res) => {
+    let {message, user} = req.body
+    messages.unshift({text: message, user: user, added: new Date()})
+    res.redirect('/')
+})
 
 module.exports = newMessageRouter;
