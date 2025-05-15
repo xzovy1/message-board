5cd69449b83833073a1dda9e971b9c4330d2633d
cabelstein12 <Caleb.Steinwandt@gmail.com>
2025-05-15 08:24:46 -0600

    clear canvas on double click for main screen

--- public/sketch.js
+++ public/sketch.js
@@ -22,3 +22,7 @@ function mouseClicked(){
     stroke(random(0,255),random(0,255),random(0,255))
     fill(random(0,255),random(0,255),random(0,255))
 }
+
+function doubleClicked(){
+    clear();
+}
